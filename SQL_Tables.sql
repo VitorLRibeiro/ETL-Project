d02376ff8003f035a35607c2f052dbdf1f8d049f
VitorRibeiro324-lab <55093247+VitorRibeiro324-lab@users.noreply.github.com>
2020-02-01 09:52:05 -0500

    Add files via upload

---
+++ SQL_Tables.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Math_06_12 (
+  dbn TEXT,
+  grade INT,
+  year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
+
+CREATE TABLE ELA_06_12 (
+  dbn TEXT,
+  grade INT,
+  year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
+
+CREATE TABLE Math_13_17 (
+  dbn TEXT,
+  grade INT,
+  year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
+
+CREATE TABLE ELA_13_17 (
+  dbn TEXT,
+  grade INT,
+  year INT,
+  number_tested INT,
+  mean_scale_score INT
+);
+
